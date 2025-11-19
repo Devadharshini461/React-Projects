326cd850d5341e9401145ecc88e0832e2503aad5
devadharshini461 <devadharshini.s461@gmail.com>
2025-11-19 14:40:54 +0530

    first commit

--- Personal Task Manager/src/Components/Navbar.jsx
+++ Personal Task Manager/src/Components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Navbar() {
+  const navigate=useNavigate()
   return (
     <div>
         <nav className='navbar' >
-        <div className='logo'><i class="fa-solid fa-list-check"></i>TOTODO</div>
+        <div className='logo' onClick={()=>{navigate('/')}}><i class="fa-solid fa-list-check"></i>TASK MANAGER</div>
         {/* <ul className='links' >
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/ProductPage'>Products</Link></li>
